966b5558029eedff59d8b3d7b969e3d8e6482662
Gaurav <gauravraj7011@gmail.com>
2025-01-31 22:47:49 +0530

    variables types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 12345
+let accountEmail = "gaurav@gmail.com"
+var accountPassword = 112233
+accountCity = "Alwar"
+
+let accountState;
+
+
+/*
+var use nhi krna
+because of issue in block scope and functional scope
+*/
+
+// accountId = 98745 // not allowed 
+
+accountEmail = "gaurav@123google.com"
+accountPassword = 65498
+accountCity = "jaipur"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountState])
