04495e9f9774d13f092470b7f3ef858f7978a0b8
Gaurav <gauravraj7011@gmail.com>
2025-02-03 19:36:33 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// Date - 03/Feb/2025
+
+// console.log(2 > 1);
+// console.log(2>=1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// === // triple equalto also check data type
+
+console.log("2" == 2); // output => true
+console.log("2" === 2); // output =>false
+
