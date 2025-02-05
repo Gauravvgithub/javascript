bdf4c40ea24f33f326a98e76f44b9efdf31fdd86
Gaurav <gauravraj7011@gmail.com>
2025-02-05 23:58:59 +0530

    summary datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,61 @@
+
+/* Date - 05 / feb / 2025*/
+
+//  Primitive Data Type
+
+
+// 7 types 
+ /*
+ 1.String
+ 2.Number
+ 3.Boolean
+ 4.Null
+ 5.Undefined
+ 6.Symbol
+ 7.BigInt
+ */
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const 
+
+
+
+
+// JavaScript is a Dynamically Type language
+
+ // Reference type (also known as Non Primitive data type)
+
+ // 1 . Array
+ // 2 . Objects
+ // 3 . Functions
+
+const hero = ["shaktiman", "naajraj", "doga"] // array
+
+let myObj={
+    name : "gaurav",
+    age : 22,
+} // Objects
+
+
+// Functions 
+
+const myFunction =  function (){
+    
+    console.log("Hello World! My name is Gaurav");
+
+}
+
+
+console.log(typeof );
