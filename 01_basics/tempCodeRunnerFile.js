0b3d9363150cb9fd098fdf31938a2f749cdad565
Gaurav <gauravraj7011@gmail.com>
2025-02-07 23:47:16 +0530

    updates

--- 01_basics/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toISOString());
-// console.log(myDate.toLocaleString());
-// console.log(myDate.toJSON());
-// console.log(myDate.toLocaleDateString());
-
-// console.log(typeof myDate);
