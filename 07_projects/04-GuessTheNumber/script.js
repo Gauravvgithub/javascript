b39c7de1aa783bb86710d6ff50d919934aac4abe
Gaurav <gauravraj7011@gmail.com>
2025-02-21 23:20:00 +0530

    project 4 inprocess

---
+++ 07_projects/04-GuessTheNumber/script.js
@@ -0,0 +1,19 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
+
+const p = document.createElement("p");
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+function validateGuess(guess){
+    
+}
